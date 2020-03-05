cf1659a57ebe8f65f95dde418038c7f7ea06098a
Ezio <40154651+Eziobro@users.noreply.github.com>
2020-03-05 12:01:27 +0800

    Update rules.js

--- bin/rules.js
+++ bin/rules.js
@@ -1,29 +1,11 @@
 exports.rules = [
     {
         rule: './blog/segmentfault_article.js',
-        url: 'https://segmentfault.com/u/ipromise/articles',
-        database: 'spider',
-        document: 'BLOG',
-        username: 'ipromise',
-        realname: '简佳成',
-        sitename: 'segmentfault',
-    },
-    {
-        rule: './blog/segmentfault_article.js',
         url: 'https://segmentfault.com/u/eizobro/articles',
         database: 'spider',
         document: 'BLOG',
         username: 'eizo',
         realname: '戴宇尘',
         sitename: 'segmentfault',
-    },
-    {
-        rule: './blog/segmentfault_article.js',
-        url: 'https://segmentfault.com/u/laoxie_59b8a26ad8c18/articles',
-        database: 'spider',
-        document: 'BLOG',
-        username: 'laoxie_59b8a26ad8c18',
-        realname: '老谢',
-        sitename: 'segmentfault',
-    },
+    }
 ];
